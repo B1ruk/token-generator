b0c6ffeb4254806835c1a67d7c1091166711595b
b1ruk <biruk3870@gmail.com>
2023-10-13 03:24:13 -0300

    initial backend package structure

---
+++ src/main/java/io/b1ruk/start/tokengenerator/TokenGeneratorApplication.java
@@ -0,0 +1,14 @@
+package io.b1ruk.start.tokengenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TokenGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TokenGeneratorApplication.class, args);
+    }
+
+
+}
---
+++ src/test/java/io/b1ruk/start/tokengenerator/TokenGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package io.b1ruk.start.tokengenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TokenGeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
